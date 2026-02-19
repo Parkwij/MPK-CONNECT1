e95ae7f208d83353dcfaf05b3a90a4616d441510
Parkwij <wijdannabil23@gmail.com>
2026-02-19 17:28:54 +0700

    fix password

--- pw.js
+++ pw.js
@@ -1,1 +1,8 @@
-const ADMIN_PASSWORD = "mpk2024";
+(function(){
+    const _k = ["bX","Br","Mg","Iy","Ng","=="].join("");
+    const _x1 = atob(_k);
+    Object.defineProperty(window, '_x1', {
+        get: function() { return _x1; },
+        configurable: false
+    });
+})();
