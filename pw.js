8d570b199fee13efd57d6c7e0f76cc40b1ebc4b2
Parkwij <wijdannabil23@gmail.com>
2026-02-19 18:08:57 +0700

    update pw

--- pw.js
+++ pw.js
@@ -1,8 +1,1 @@
-(function(){
-    const _k = ["bX","Br","Mg","Iy","Ng","=="].join("");
-    const _x1 = atob(_k);
-    Object.defineProperty(window, '_x1', {
-        get: function() { return _x1; },
-        configurable: false
-    });
-})();
+window._x1 = atob("bXBrMjAyNg==");
